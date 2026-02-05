1757c2230543bd59ebd4805b371fc98dc900ba7c
Miguel <miguel.meki@gmail.com>
2026-02-05 17:44:25 +0100

    Update series.py

--- api/series.py
+++ api/series.py
@@ -13,7 +13,7 @@ from huggingface_hub import hf_hub_download
 # ----------------------------
 # CONFIG (EDITA ESTO)
 # ----------------------------
-HF_REPO_ID = "TU_USUARIO_HF/sig-riego-rdc-raw"  # <-- CAMBIA ESTO
+HF_REPO_ID = "miguel-byte-breath/sig-riego-rdc-raw"
 NC_FILES = [
     "cds_fc_2023_2024.nc",
     "cds_fc_2025_2025.nc",
